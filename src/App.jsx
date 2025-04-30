ab1c0ff2410206a71d333e74cc4b79e915503aca
Upptech1 <upptech1@gmail.com>
2025-04-30 13:13:30 +0200

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// Import Pages
 import StartPage from './pages/startPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <StartPage/>
